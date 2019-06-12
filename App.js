c34c5a568debe8f770a57fa5d3e2b51ce81ba51f
Liem Nguyen <liemnguyen974@gmail.com>
2019-06-11 17:35:11 -0700

    props

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Alex' />
+        <Greeting name='Rose' />
+        <Greeting name='Sam' />
       </View>
     );
   }
